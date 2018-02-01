312c83ab36bec205c670f1dfee6ba389c0897fd5
Victor Sharamet <sharamet@gmail.com>
2018-02-02 02:43:45 +0300

    header is ready
    nav js issue isnt soled

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 
-	/* Call plugins hrere ...*/
 
-	
-});
+	//slide2id - плавная прокрутка по ссылкам внутри страницы
+	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
+	    highlightSelector:"nav a"
+	});
+
+
+}); 
